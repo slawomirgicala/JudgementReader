4e6d47c571f16a34247a517b498946602a937c50
sgica <sgicala98@gmail.com>
2018-12-28 21:53:51 +0100

    app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 
     public static void main(String[] args){
-
-
-
+        Map judgments = new HashMap<Integer,Judgment>();
+        new JsonParser("C:\\Users\\sgica\\Desktop\\GoodbyeWorld\\semestr3\\obiektowe\\json\\judgments-348.json").parse(judgments);
+        new HtmlParser("C:\\Users\\sgica\\Desktop\\GoodbyeWorld\\semestr3\\obiektowe\\html\\04\\01\\70D462EECA.html").parse(judgments);
+        JudgmentQueries queries = new JudgmentQueries(judgments);
+        InteractiveApp app = new InteractiveApp(queries);
+        app.run();
     }
 }
